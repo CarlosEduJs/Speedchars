795b373af67098d42ddedd9115cc1a158fab3cb9
Carlos Eduardo <carloseduardogit@gmail.com>
2025-02-16 14:35:05 -0300

    ajusted reportType: request

--- src/app/api/send-report/route.ts
+++ src/app/api/send-report/route.ts
@@ -35,7 +35,7 @@ export async function POST(request: Request) {
 
     if (
       !reportType ||
-      !["problem", "suggestion", "feature_request", "other"].includes(
+      !["problem", "suggestion", "request", "other"].includes(
         reportType
       )
     ) {
